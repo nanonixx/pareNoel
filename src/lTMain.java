b6526ee5652a646788f519509e61ddef5650c877
Naomi <nhid@elpuig.xeill.cat>
2021-01-12 17:55:11 +0100

    me dan errores sin ningún sentido

--- src/lTMain.java
+++ src/lTMain.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main {
+public class lTMain {
     public static void main(String[] args) throws FileNotFoundException {
         File santako = new File("src/santako.txt");
 
